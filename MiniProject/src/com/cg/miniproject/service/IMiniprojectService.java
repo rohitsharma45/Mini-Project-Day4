ef06c9beace6ac069b74ab95ca25d18a686ee83b
rohitsharma45 <32483973+rohitsharma45@users.noreply.github.com>
2017-10-20 18:26:30 +0530

    Add files via upload

---
+++ MiniProject/src/com/cg/miniproject/service/IMiniprojectService.java
@@ -0,0 +1,26 @@
+package com.cg.miniproject.service;
+
+import java.util.ArrayList;
+
+import com.cg.miniproject.bean.MacBean;
+import com.cg.miniproject.bean.MiniprojectBean;
+
+public interface IMiniprojectService {
+
+	int insertData(MiniprojectBean bean);
+
+	String getProgramId();
+
+	boolean checkLogin(MacBean mac);
+
+	boolean checkAdminLogin(MacBean mac);
+
+	String getId(String string);
+
+	ArrayList<String> retrieveDetails();
+
+	ArrayList<MiniprojectBean> getApplicantList(String programName);
+
+	//boolean validateUser(ArrayList<MacBean> list);
+
+}
